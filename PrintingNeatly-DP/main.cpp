2fb033f6ed6f4fa34dc9f894c61cdc15b468b721
EucliTs0 <dimitri_ts0@hotmail.com>
2016-11-05 18:29:39 +0100

    Update main.cpp

--- PrintingNeatly-DP/main.cpp
+++ PrintingNeatly-DP/main.cpp
@@ -1,19 +1,47 @@
-#include<iostream>
-#include<cstring>
-#include<cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 #include <string>
 #include <vector>
-#include <limits.h>
 #include <fstream>
 #include <stdio.h>
 #include <fstream>
+#include <stdlib.h>
+#include <cctype>
+#include <math.h>
+#include <cmath>
 
 using namespace std;
 
-void wrap(vector<string>, int);
+/* char* lcs2(char *, char *);
+void lcs(char *, char *);
+
+int main()
+{
+  char x[] = "ABBDDBDGG";
+  char y[] = "GBDBAGB";
+
+
+  string result;
+
+  result = lcs2(x, y);
+
+  cout << "LCS using lcs2 function is : " << result;
+  cout << "\n";
+  cout << "LCS using lcs function is  : " ;
+  lcs(x, y);
+
+  return 0;
+}
+*/
+
+void printNeatly (vector<string> , int);
+int giveLines(int *, int, vector<string>, int);
+
 int main() {
 
-    int colNum = 25;
+    int M = 30;
+
     ifstream mytextfile;
 
     try {
@@ -30,13 +58,13 @@ int main() {
     }
 
     string word;
-    for (int k = 0; k < colNum; k++){
+    for (int k = 0; k < M; k++){
         cout << k % 10;
     }
     cout << "   | spaces" << endl;
     cout << "<";
 
-    for (int k = 0; k < colNum - 1; k++) {
+    for (int k = 0; k < M - 2; k++) {
         cout << "-";
     }
     cout << ">  |" << endl;
@@ -45,8 +73,9 @@ int main() {
     vector<string> words;
     while (mytextfile >> word) {
             cout << word << endl;
-        if (word.size() > colNum) {
-            cout << "Words are larger than the size of columns " << endl;
+
+        if (word.size() > M) {
+            cout << "Words are larger than the size of M " << endl;
             cin.get();
             return 1;
 
@@ -54,12 +83,20 @@ int main() {
         words.push_back(word);
         cout << int(words.size()) << endl;
     }
+
+
+
     cout << endl;
 
-    mytextfile.close();
-    return 0;
 
-    wrap(words, colNum);
+
+    mytextfile.close();
+
+
+
+    printNeatly(words, M);
+
+
 
     cout << "Press Enter to exit " << endl;
     cin.get();
