d30856d113de939201b3968f017cf8c2c0be4b17
EucliTs0 <dimitri_ts0@hotmail.com>
2016-11-05 18:28:04 +0100

    Update printingNeatlyDP.cpp

--- PrintingNeatly-DP/printingNeatlyDP.cpp
+++ PrintingNeatly-DP/printingNeatlyDP.cpp
@@ -1,108 +1,102 @@
-#include<iostream>
-#include<cstring>
-#include<cstdlib>
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <stdio.h>
 #include <fstream>
+#include <stdlib.h>
+#include <cctype>
+#include <math.h>
+#include <cmath>
 
-/* INT_MAX defines the maximum number an int can have */
+
 #define MAXINT INT_MAX
 
 using namespace std;
 
-int printingNeatly(int ps[], int n, vector<string> words, int colNum){
-        int x;
-        if (ps[n] == 1)
-            x = 1;
-        else
-            x = printingNeatly(ps, ps[n], words, colNum) + 1;
-        int s = 0; /* size */
-        int wordsNumInLine = 0;
-        for (int i = ps[n]; i <= n; i++){
-            s += words[i - 1].size();
-            cout << words[i - 1] << " ";
-            wordsNumInLine++;
-        }
-        int spaces = colNum - s;
-        if (wordsNumInLine > 1){
-            spaces = spaces - (wordsNumInLine - 1);
-        }
-        cout << std::string(spaces, ' ') << " |  " << spaces << endl;
-        return x;
 
-}
+/* M is the width of every line printed
 
-void wrap(vector<string> words, int colNum){
-    int s_words = words.size();
+   n is the length of words */
 
-    /* We need to define spaces in a table where we store the words
-    from i to j in a single line */
+int giveLines(int *, int, vector<string>, int);
 
-    int tableSpace[s_words + 1][s_words + 1];
+void printNeatly(vector<string> words, int M){
+    /* Let extras[1...n, 1...n], lc[1...n, 1...n] and c[0...n]
+    be new arrays */
 
-    /* Then, we need to define a table where we put the cost
-    of a line from words i to j */
+    int n = words.size();
 
-    int costLine[s_words + 1][s_words + 1];
-
-    /* Last, a table to save the total cost */
-
-    int totalCost[s_words + 1];
-
-    /* Array to print the solution */
-    int print_sol[s_words + 1];
-
-    int t, u;
-
-    /* Find extra spaces in a single line */
-
-    for (t = 1; t <= s_words; t++){
-        /* Calculate number of spaces */
-        tableSpace[t][t] = colNum - words[t - 1].size();
-        for (u = 1; u <= s_words; u++){
-            tableSpace[t][u] = tableSpace[t][u - 1] - words[u - 1].size() - 1;
-
-        }
-
-    }
-
-    /* Calculate cost based on spaces */
-    for (t = 1; t <= s_words; t++ ) {
-        for (u = t; u <= s_words; u++) {
-            /* If we have a huge number of words
-            in a single line */
-            if (tableSpace[t][u] < 0)
-                costLine[t][u] = MAXINT;
-
-                /* Do not count the cost of the last line */
-                else if (u == s_words && tableSpace[t][u] >= 0)
-                    costLine[t][u] = 0;
-
-                /* Cost of each line is tableSpace[i][j]^3 */
-                        else
-                            costLine[t][u] = tableSpace[t][u]*tableSpace[t][u]*tableSpace[t][u];
+    int extras[n + 1][n + 1]; /* white spaces in a line */
+    int lc[n + 1][n + 1]; /* Line Cost */
+    int c[n + 1]; /* Total Cost */
+    int p[n + 1];
 
 
+    /* Compute extras[i,j] for 1 <= i <= j <= n */
+    int i;
+    int j;
+
+    for (i = 1; i <= n; i++) {
+        extras[i][i] = M - words[i - 1].size();
+        for (j = i + 1; j <= n; j++) {
+            extras[i][j] = extras[i][j - 1] - words[j - 1].size() - 1;
         }
     }
 
-    totalCost[0] = 0;
-    for (u = 1; u <= s_words; u++) {
-        totalCost[u] = MAXINT;
+    /* Compute lc[i, j] for 1 <= i <= j <= n */
+    for (i = 1; i <= n; i++){
+        for (j = i; j <= n; j++) {
+            if (extras[i][j] < 0)
+                lc[i][j] = MAXINT;
+            else if (j == n && extras[i][j] >= 0)
+                lc[i][j] = 0;
+            else
+                lc[i][j] = pow(extras[i][j], 3);
+        }
+    }
 
-        for (t = 1; t <= u; t++) {
-            if (totalCost[t - 1] != MAXINT && costLine[t][u] != MAXINT && (totalCost[t - 1] + costLine[t][u] < totalCost[u])) {
-                totalCost[u] = totalCost[t - 1] + costLine[t][u];
-                print_sol[u] = t;
+    /* Compute c[j] and p[j] for 1 <= j <= n */
+    c[0] = 0;
+    for (j = 1; j <= n; j++){
+        c[j] = MAXINT;
+
+        for (i = 1; i <= j; i++){
+            if ((c[i - 1] + lc[i][j]) < c[j] && c[i - 1] != MAXINT && lc[i][j] != MAXINT) {
+                c[j] = c[i - 1] + lc[i][j];
+                p[j] = i;
             }
         }
     }
+    giveLines(p, n, words, M);
+    	cout << "Total Cost (Penalty): " << c[n] << endl;
+}
 
-    printingNeatly(print_sol, s_words, words, colNum);
-    cout << "Penalty: " << totalCost[s_words] << endl;
+int giveLines (int p[], int n, vector<string> words, int M) {
+    int i, k;
+
+    if (p[n] == 1)
+        k = 1;
+    else
+        k = giveLines(p, p[n] - 1, words, M) + 1;
+        int s  = 0; /* size */
+        int wordsInLine = 0;
+        for (i = p[n]; i <=n; i++) {
+            s += words[i + 1].size();
+            cout << words[i - 1] << " ";
+            wordsInLine++;
+        }
+        int spaces = M - s;
+        if (wordsInLine > 1) {
+            spaces = spaces - (wordsInLine - 1);
+        }
+        cout << std::string(spaces, ' ') << " |  " << spaces << endl;
+
+        return k;
 
 }
+
 
 
